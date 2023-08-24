cf278476836690ccd47f1b88a269755136a08947
MDMahidul <mmbmahidul007@gmail.com>
2023-08-24 23:16:08 +0600

    created react app and install few dependencis also created components and add some css clsses

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router}></RouterProvider>
+  </>
+);
